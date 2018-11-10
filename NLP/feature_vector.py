908ef3987cc15606feed9418cd97018ec20bfb8b
Jonathan <Jonathan Young>
2018-11-09 22:30:13 -0330

    Add TF-IDF calculations

--- NLP/feature_vector.py
+++ NLP/feature_vector.py
@@ -4,16 +4,19 @@ import numpy as np
 from NLP.ngrams import ngramParser
 from NLP.spellcheck import exportCache
 from NLP.sentenceParser import SentenceParser
+from NLP.tfidf import TFIDF
 
 class FeatureVectorGenerator(object):
 
     def __init__(self):
         self.parser = ngramParser()
         self.sentence_parser = SentenceParser()
+        self.tf_idf = TFIDF()
     
     def cache(self):
         self.sentence_parser.cacheScores()
         exportCache()
+        self.tf_idf.cacheScores()
 
     def getFeatureVector(self, text):
 
@@ -22,11 +25,13 @@ class FeatureVectorGenerator(object):
         sentiment_score = sentiment_values[0]
         sentiment_objectivity = sentiment_values[1]
         sentence_score = self.sentence_parser.parseSentences(text)
+        tf_idf_score = self.tf_idf.get_score(text)
 
         vector =    [
                         (ngram_vector["Good"]-ngram_vector["Bad"])*(ngram_vector["Second-Person"]),
                         ngram_vector["Bad"],
                         sentence_score,
+                        tf_idf_score,
                         sentiment_score*sentiment_objectivity,
                         ngram_vector["Second-Person"],
                     ]
